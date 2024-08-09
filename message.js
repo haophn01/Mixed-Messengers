ab046bbd495c52e479c8efc2602bcdc508cc93bf
Hao Phan <rinnphan01@gmail.com>
2024-08-09 11:38:09 -0700

    Update wisdom for message.js

---
+++ message.js
@@ -0,0 +1,7 @@
+const wisdom = {
+    fortune: ["luck", "bad", "worse", "terible", "ok luck", "good luck"], 
+    advice: ["Keep calm", "trust no one", "enjoy life more", "go out more for your social life"]
+}
+
+
+
